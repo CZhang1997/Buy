699d298b9f95ca7c3e6bb9cf57acf95eef8cbb22
Churong Zhang <churong.zhang@utdallas.edu>
2019-01-20 00:49:38 -0600

    Add Labels

--- Buy?/ViewController.swift
+++ Buy?/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fromCurrency: UITextField!
+    @IBOutlet weak var toCurrency: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
