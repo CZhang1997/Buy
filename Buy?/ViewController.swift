a0d9053a6aa7a70aa2e44246eae16bd75f4a7bd9
Churong Zhang <churong.zhang@utdallas.edu>
2019-01-20 04:04:21 -0600

    Setup API and access online currency data

--- Buy?/ViewController.swift
+++ Buy?/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SVProgressHUD
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
 
 
     let regionArray = ["纽约", "洛杉矶", "旧金山", "达拉斯", "芝加哥", "休斯敦", "西雅图", "迈阿密", "波士顿"]
+    let currencyCode = ["CNY","other"]
+    let url = "http://apilayer.net/api/live"
+    let accessCode = ""
+    
+    var currencyJSON : JSON?
+    var input : Double?
     
     
     @IBOutlet weak var exchangeButton: UIButton!
@@ -38,13 +47,52 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         // Do any additional setup after loading the view, typically from a nib.
         RegionPicker.delegate = self
         RegionPicker.dataSource = self
-        exchangeButton.setTitle("ExChange To", for: .normal)
+        exchangeButton.setTitle("ExChange", for: .normal)
     }
 
     @IBAction func ExchangePressed(_ sender: UIButton) {
         print ("Pressed exchange")
+        if let usd = Double(USDTextField.text!) {
+            input = usd
+            print ("double")
+            getCurrencyData()
+        }
+        else
+        {
+            CNYLabel.text = "enter number only"
+        }
         
     }
+    
+    func calculate()
+    {
+        if let USDCNY = currencyJSON!["quotes"]["USDCNY"].double {
+            let change = input! * USDCNY
+            CNYLabel.text = "\(change)"
+            
+        }
+    }
+    
+    func getCurrencyData()
+    {
+        
+        let params: [String:String] = ["access_key": accessCode, "format" : "1"]
+        
+        Alamofire.request(url, method: .get, parameters: params).responseJSON { respond in
+            if respond.result.isSuccess {
+                //print (respond)
+                self.currencyJSON = JSON(respond.result.value!)
+                self.calculate()
+            }
+            else{
+                print ("connection issue")
+                print (respond)
+            }
+            
+        }
+    }
+    
+    
     
 }
 
