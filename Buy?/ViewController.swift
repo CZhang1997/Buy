a4be593d709e7222d363bece98e7e17fc853d7b8
Churong Zhang <churongzhang@Deborahs-MBP.frontierlocal.net>
2019-01-20 00:15:14 -0600

    First Commit

---
+++ Buy?/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Buy?
+//
+//  Created by Churong Zhang on 1/19/19.
+//  Copyright © 2019 Churong Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    @IBAction func ExchangePressed(_ sender: UIButton) {
+        print ("Pressed")
+    }
+    
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+
+target 'Buy?' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Buy?
+    pod 'SwiftyJSON'
+    pod 'Alamofire'
+    pod 'SVProgressHUD'
+
+end
