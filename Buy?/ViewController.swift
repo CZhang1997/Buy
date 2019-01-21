06c17b3f4407d8f47e63c307446dda854c746381
Xizhen Yang <xizhenyang@XizhendeMBP.frontierlocal.net>
2019-01-20 19:44:58 -0600

    Update state and city selector

--- Buy?/ViewController.swift
+++ Buy?/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
-    let stateArray = ["AZ-亚利桑那州", "CA-加利福尼亚州", "CO-科罗拉多州", "FL-佛罗里达州", "GA-乔治亚州", "HI-夏威夷州", "IL-i伊利诺伊州", "MD-马里兰州", "MA-马萨诸塞州", "MI-密歇根州", "NV-内华达州", "NY-纽约州", "OR-俄勒冈州", "TX-德克萨斯州", "WA-华盛顿州"]
+    let stateArray = ["AZ-亚利桑那州", "CA-加利福尼亚州", "CO-科罗拉多州", "FL-佛罗里达州", "GA-乔治亚州", "HI-夏威夷州", "IL-伊利诺伊州", "MD-马里兰州", "MA-马萨诸塞州", "MI-密歇根州", "NV-内华达州", "NY-纽约州", "OR-俄勒冈州", "TX-德克萨斯州", "WA-华盛顿州"]
     let cityArray = ["纽约", "洛杉矶", "旧金山", "达拉斯", "芝加哥", "休斯敦", "西雅图", "迈阿密", "波士顿"]
     let AZ = ["凤凰城"]
     let CA = ["旧金山", "洛杉矶"]
@@ -30,7 +30,8 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
     
     var currentTextField = UITextField()
     var pickerView = UIPickerView()
-    var i : Int = 0
+    var i : Int = 0     // State Counter.
+    var chooseCity : String = ""
     
     @IBOutlet weak var USDTextField: UITextField!
     @IBOutlet weak var CNYLabel: UILabel!
@@ -47,24 +48,8 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
         if currentTextField == stateTextField {
             return stateArray.count
         } else if currentTextField == cityTextField {
-            switch i {
-                case 0: return AZ.count
-                case 1: return CA.count
-                case 2: return CO.count
-                case 3: return FL.count
-                case 4: return GA.count
-                case 5: return HI.count
-                case 6: return IL.count
-                case 7: return MD.count
-                case 8: return MA.count
-                case 9: return MI.count
-                case 10: return NV.count
-                case 11: return NY.count
-                case 12: return OR.count
-                case 13: return TX.count
-                case 14: return WA.count
-                default: return 0
-            }
+            let choosenState : [String] = identifyState(stateCounter : i)
+            return choosenState.count
         } else {
             return 0
         }
@@ -74,24 +59,8 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
         if currentTextField == stateTextField {
             return stateArray[row]
         } else if currentTextField == cityTextField {
-            switch i {
-                case 0: return AZ[row]
-                case 1: return CA[row]
-                case 2: return CO[row]
-                case 3: return FL[row]
-                case 4: return GA[row]
-                case 5: return HI[row]
-                case 6: return IL[row]
-                case 7: return MD[row]
-                case 8: return MA[row]
-                case 9: return MI[row]
-                case 10: return NV[row]
-                case 11: return NY[row]
-                case 12: return OR[row]
-                case 13: return TX[row]
-                case 14: return WA[row]
-                default: return nil
-            }
+            let choosenState : [String] = identifyState(stateCounter : i)
+            return choosenState[row]
         } else {
             return ""
         }
@@ -99,26 +68,12 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         if currentTextField == stateTextField {
-            switch row {
-                case 0: i = 0
-                case 1: i = 1
-                case 2: i = 2
-                case 3: i = 3
-                case 4: i = 4
-                case 5: i = 5
-                case 6: i = 6
-                case 7: i = 7
-                case 8: i = 9
-                case 9: i = 9
-                case 10: i = 10
-                case 11: i = 11
-                case 12: i = 12
-                case 13: i = 13
-                case 14: i = 14
-                default: i = 0
-            }
+            i = row
+            stateTextField.text = stateArray[row]
             self.view.endEditing(true)
         } else if currentTextField == cityTextField {
+            let choosenState : [String] = identifyState(stateCounter : i)
+            cityTextField.text = choosenState[row]
             self.view.endEditing(true)
         }
     }
@@ -142,5 +97,37 @@ class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegat
         print ("Pressed")
     }
     
+    
+    
+    
+    
+    func identifyState(stateCounter : Int) -> [String] {
+        switch stateCounter {
+            case 0: return AZ
+            case 1: return CA
+            case 2: return CO
+            case 3: return FL
+            case 4: return GA
+            case 5: return HI
+            case 6: return IL
+            case 7: return MD
+            case 8: return MA
+            case 9: return MI
+            case 10: return NV
+            case 11: return NY
+            case 12: return OR
+            case 13: return TX
+            case 14: return WA
+            default: return AZ
+        }
+        
+    }
+    
+    
+    
 }
+
+
+
+
 
